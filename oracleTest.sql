28ecf99004047a8fdfc3fc96a23dadbf72171747
Leitor8 <andrefelipe.costasilva@gmail.com>
2025-05-27 09:27:52 -0300

    feat:first iteration of database file

---
+++ oracleTest.sql
@@ -0,0 +1,6 @@
+BEGIN
+    SELECT 'knockout'   FROM dual UNION ALL
+    SELECT 'angularjs'  FROM dual UNION ALL
+    SELECT 'jquery'     FROM dual UNION ALL
+    SELECT 'underscore' FROM dual;
+END;
